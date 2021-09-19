c22ae410c8f955458ed03a72648c31641cf334a8
ByoneCode <2488068072@qq.com>
2021-09-19 11:39:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vue 3 + Typescript + Vite
+
+## Install
+```git
+  git clone https://github.com/ByoneCode/My-TodoList.git
+```
+## Usage
+```yarn
+  cd My-TodoList
+  yarn install / npm install
+  yarn run dev / npm run dev
+```
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { VitePWA } from 'vite-plugin-pwa'
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir);
+}
+
+
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({})
+  ],
+  resolve: {
+    alias: [
+      {
+        find: /\/@\//,
+        replacement: pathResolve('src') + '/',
+      }
+    ]
+  }
+})
