f16e593b5a266eeb0e0fcc75b9467ee075d7f65e
Tirzen <TirzenLee@Foxmail.com>
2021-09-26 02:25:36 +0800

    css for global

--- src/layout/components/nav/index.vue
+++ src/layout/components/nav/index.vue
@@ -1,68 +1,71 @@
 <template>
-    <aside class="container__sidebar" :class="{open_side:isopen}" @click="emit('closeSide')">
-      <div class="sidebar">
-        <div class="side_user">
-            <div class="avatar">
-              <span>杰</span>
-            </div>
-            <div class="info">
-              <p>ByoneJie</p>
-              <p>2488068072@qq.com</p>
-            </div>
-          <div class="search">
-            <i class="iconfont icon-theme"></i>
-          </div>
+  <aside
+    class="container__sidebar"
+    :class="{ open_side: isopen }"
+    @click="emit('closeSide')"
+  >
+    <div class="sidebar">
+      <div class="side_user">
+        <div class="avatar">
+          <span>杰</span>
         </div>
-        <side-nav
-        :list="stat.navList"
-        ></side-nav>
-        <div class="side_hr"></div>
-        <side-list></side-list>
-        <div class="side_add">
-          <i class="iconfont icon-add"></i>
-          <span>新建列表</span>
+        <div class="info">
+          <p>ByoneJie</p>
+          <p>2488068072@qq.com</p>
+        </div>
+        <div class="theme">
+          <i class="iconfont icon-theme"></i>
         </div>
       </div>
-    </aside>
+      <side-nav :list="stat.navList"></side-nav>
+      <div class="side_hr"></div>
+      <side-list></side-list>
+      <div class="side_add">
+        <i class="iconfont icon-add"></i>
+        <span>新建列表</span>
+      </div>
+    </div>
+  </aside>
 </template>
 
 <script setup lang="ts">
-import SideNav from './SideNav.vue'
-import SideList from './SideList.vue'
+import SideNav from "./SideNav.vue";
+import SideList from "./SideList.vue";
 
-import { reactive } from 'vue'
+import { reactive } from "vue";
 const props = defineProps({
   isopen: {
     type: Boolean,
-    default: false
-  }
-})
+    default: false,
+  },
+});
 
 const stat = reactive({
   navList: [
-  {
-    title: '星标',
-    icon: 'star',
-    path: '/star/index',
-    hidden: false
-  },
-  {
-    title: '便笺',
-    icon: 'note',
-    path: '/note/index',
-    hidden: false
-  },
-  {
-    title: '任务',
-    icon: 'home',
-    path: '/home/index',
-    hidden: false
-  }]
-})
+    {
+      title: "星标",
+      icon: "star",
+      path: "/star/index",
+      hidden: false,
+    },
+    {
+      title: "便笺",
+      icon: "note",
+      path: "/note/index",
+      hidden: false,
+    },
+    {
+      title: "任务",
+      icon: "home",
+      path: "/home/index",
+      hidden: false,
+    },
+  ],
+});
 
-const emit = defineEmits(['closeSide'])
+const emit = defineEmits(["closeSide"]);
 </script>
 
 <style lang="less" scoped>
-  @import "./index";
+@import "./index";
 </style>
