89f8fae53f2271d732c7a00fa85e78a083e501dd
RobinPang <pcz0212@outlook.com>
2021-09-28 05:15:24 +0800

    便笺初改

--- src/views/note/index.vue
+++ src/views/note/index.vue
@@ -21,17 +21,13 @@
         </div> -->
         <div class="note-list-item" :class="{edit:item.isEdit}" v-for="(item,index) in stat.list" @click="toggleEdit(index)">
           <div class="note-toolbar">
-            <!-- <div class="note-add">
-              <i class="iconfont icon-add"></i>
-            </div> -->
             <div class="note-star">
               <i class="iconfont icon-star"></i>
             </div>
             <div class="note-del">
-              <i class="iconfont icon-del"></i>
+              <i class="iconfont icon-delete"></i>
             </div>
           </div>
-
           <div class="note-content">
             <p>
               {{item.text}}
@@ -49,11 +45,11 @@ const stat = reactive({
   isEdit: false,
   list: [
     {
-      text: ' 这是一个记事本可以用来为源响应式对象上的某个 property 新创建一个ref。然后，ref 可以被传递，它会保持对其源 property的响应式连接即使源 property 不存在，toRef 也会返回一个可用的ref。这使得它在使用可选 prop 时特别有用，可选 prop 并不会被 toRefs处理。',
+      text: '滚滚长江东逝水，浪花淘尽英雄。是非成败转头空。青山依旧在，几度夕阳红。白发渔樵江渚上，惯看秋月春风。一壶浊酒喜相逢。古今多少事，都付笑谈中。',
       isEdit: false
     },
     {
-      text: '这是一个便签内容',
+      text: `La trompe de l'éléphant, c'est pour ramasser les pistaches: pas besoin de se baisser. Le cou de la girafe, c'est pour brouter les astres: pas besoin de voler. La peau du caméléon, verte, bleue, mauve, blanche, selon sa volonté, c'est pour se cacher des animaux voraces: pas besoin de fuir. La carapace de la tortue, c'est pour dormir à l'intérieur, même l'hiver: pas besoin de maison. Le poème du poète, c'est pour dire tout cela et mille et mille et mille autres choses: pas besoin de comprendre.`,
       isEdit: false
     },
     {
