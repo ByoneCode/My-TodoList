11393b71439362dc5d11a2a89ca61adbe20c7d67
Tirzen <TirzenLee@Foxmail.com>
2021-09-29 21:11:41 +0800

    登陆页面，图标文字对齐优化，弹出菜单样式优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,32 @@
 <template>
-  <div>登录模板</div>
+  <div class="login">
+    <form action="">
+      <div class="login-user">
+        <i class="iconfont icon-people"></i>
+        <input type="text" placeholder="用户名或邮件地址">
+      </div>
+      <div class="login-password">
+        <i class="iconfont icon-password"></i>
+        <input type="password" placeholder="密码">
+      </div>
+      <!-- <div class="login-remember">
+        <input type="checkbox" class="remember">
+        <span>记住我</span>
+      </div> -->
+      <div class="login-btn">
+        <input type="button" value="登录">
+      </div>
+      <div class="login-reg">
+        <span>立即注册</span>
+      </div>
+    </form>
+  </div>
+    
 </template>
 
 <script setup lang="ts">
-  
 </script>
 
 <style lang="less" scoped>
-
+@import "./style";
 </style>
