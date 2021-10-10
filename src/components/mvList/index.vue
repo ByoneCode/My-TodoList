fb2d6a55d6516f5e4a909d2d2f31da66da92e080
Tirzen <TirzenLee@Foxmail.com>
2021-10-10 22:10:47 +0800

    Update index.vue

--- src/components/mvList/index.vue
+++ src/components/mvList/index.vue
@@ -1,13 +1,9 @@
 <template>
   <teleport to="#app">
     <div class="mv-list" v-show="isShow" :style="pos">
-      <div class="mv-title">
-        移动到
-      </div>
+      <div class="mv-title">移动到</div>
       <template v-for="item in stat.list">
-        <div class="mv-item">
-          {{item.title}}
-        </div>
+        <div class="mv-item">{{ item.title }}</div>
       </template>
     </div>
   </teleport>
@@ -32,23 +28,53 @@ const props = defineProps({
 })
 const stat = reactive({
   list: [
-  {
-    title: '测试列表1'
-  },
-  {
-    title: '测试列表2'
-  }
+    {
+      title: '测试列表1'
+    },
+    {
+      title: '测试列表2'
+    }
   ]
 })
 </script>
 
 <style lang="less" scoped>
-.mv-list{
+.mv-list {
   position: fixed;
   width: 150px;
-  background-color: azure;
+  background-color: @bg-color-items;
   top: 0;
   left: 0;
   z-index: 99;
+
+  display: flex;
+  flex-direction: column;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  padding: 0.5rem;
+  user-select: none;
+
+  .mv-title {
+    padding: 0.5rem 0.5rem;
+    font-weight: bold;
+    color: @text-color-black;
+  }
+
+  .mv-item {
+    padding: 0.5rem 0.5rem;
+    color: @text-color-grey;
+    border-radius: 2px;
+    .ease();
+
+    &:hover {
+      background-color: @bg-color-items-hl;
+      .ease();
+    }
+
+    &:active {
+      background-color: @bg-color-items-ac;
+      .ease();
+    }
+  }
 }
 </style>
